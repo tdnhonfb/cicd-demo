6b04cf4634f49b9441e6e55df47734c3fc797040
tdnhonfb <nhon.truong@fastboy.net>
2023-02-28 23:55:39 +0700

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const util = require('util')
 const {add,minus} = require('./functions')
 
 console.log('hello worlds');
-console.log('Today is a beautiful days.')
+console.log('Today is a beautiful day.')
 var a = 2
 var b = 9
 console.log(util.format("Add: %d+%d=%d", a, b, add(a,b)))
