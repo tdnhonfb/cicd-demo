8a09f1b195e539a019cf9d7ba1bedfa2f46de94a
tdnhonfb <nhon.truong@fastboy.net>
2023-03-01 21:32:34 +0700

    new change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-  
+  console.log('ooo');
   return (
-        <p>
-          Edit <code>src/App.js</code> and save to reloadss.
-        </p>
-      
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <h1> Hello ReactJs! </h1>
+      <a className="App-link" href="https://www.youtube.com/channel/UC64HFdJWvwuNt2ruywWvF-w">Youtube Channel - Software Engineer </a>
+      </header>
+    </div>
   );
 }
 
